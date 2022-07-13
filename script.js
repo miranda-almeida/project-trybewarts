45ea57ad669efe53bfed2110358bb7e4f1268789
miranda <magui@duck.com>
2022-07-13 16:22:49 -0300

    requisito 3: adiciona inputs de login

---
+++ script.js
@@ -0,0 +1,15 @@
+const submitBtn = document.getElementById('enviar');
+
+function enviar() {
+  const username = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const correctUser = 'tryber@teste.com';
+  const correctPassword = '123456';
+  if (username === correctUser && password === correctPassword) {
+    alert('Olá Tryber!');
+  } else {
+    alert('Email ou senha inválidos');
+  }
+}
+
+submitBtn.addEventListener('click', enviar);
